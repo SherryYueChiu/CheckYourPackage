f449a1ea0e2e5bfd41ebb950e996670a027c75a7
SherryYueChiu <sherryyue.c@protonmail.com>
2021-05-07 22:56:19 +0800

    update letters

--- data.js
+++ data.js
@@ -1,4 +1,18 @@
-lastUpdate = "~ 2021/05/05 6AM";
+lastUpdate = "~ 2021/05/08 6AM";
 packages = `
+From:彰化銀行
+To:吳韻哲
+Room:501
 
+From:元大金控
+To:吳文軒
+Room:408
+
+From:元大金控
+To:陳玉琳
+Room:306
+
+From:仁壽製藥
+To:陳玉琳
+Room:306
 `;
