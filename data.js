557d6ea472c1e8c69e898a92d2c5f085eccb7d0a
SherryYueChiu <sherryyue.c@protonmail.com>
2021-05-14 21:13:42 +0800

    update letter

--- data.js
+++ data.js
@@ -1,12 +1,11 @@
-lastUpdate = "~ 2021/05/13 6AM";
+lastUpdate = "~ 2021/05/15 6AM";
 packages = `
-From:華南證券
-To:鄧翔文
-Room:508
+From:就服處
+To:陳長葦
 
-From:東海大學註冊組
-To:林鼎強
-Room:4F
+From:玉山銀行
+To:
+Room:
 
 From:彰化銀行
 To:吳韻哲
@@ -17,10 +16,6 @@ To:吳文軒
 Room:408
 
 From:元大金控
-To:陳玉琳
-Room:306
-
-From:仁壽製藥
 To:陳玉琳
 Room:306
 `;
