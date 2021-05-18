3184b3db40f7a2bdbf1ea0e83247f2ee1b06f838
SherryYueChiu <sherryyue.c@protonmail.com>
2021-05-19 01:31:08 +0800

    update letter

--- data.js
+++ data.js
@@ -1,21 +1,6 @@
 lastUpdate = "~ 2021/05/19 6AM";
 packages = `
-From:新庄郵局
-To:許呈祥
-
-From:花旗銀行
-To:葉明陽
-Room:511
-
-From:新光銀行
-To:葉明陽
-Room:511
-
 From:建準電機
-To:吳文軒
-Room:408
-
-From:元大證券
 To:吳文軒
 Room:408
 
