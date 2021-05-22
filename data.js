2f4aa88b8a8a8129aede441fc215fbc281ce6b70
Sherry <sherryyue.c@protonmail.com>
2021-05-23 01:07:45 +0800

    update letter

--- data.js
+++ data.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-lastUpdate = "~ 2021/05/19 6AM";
-=======
-lastUpdate = "~ 2021/05/17 7AM";
->>>>>>> branch 'master' of https://github.com/SherryYueChiu/CheckYourPackage
+ƕlastUpdate = "~ 2021/05/22 6AM";
 packages = `
-<<<<<<< HEAD
-From:新庄郵局
-To:許呈祥
-
-From:新光銀行
-To:葉明陽
-Room:5F
-
 From:建準電機
 To:吳文軒
 Room:408
 
-=======
->>>>>>> branch 'master' of https://github.com/SherryYueChiu/CheckYourPackage
 From:就服處
 To:陳長葦
 
