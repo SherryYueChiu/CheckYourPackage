ed083e37f627914825f42a17fcaa58baed1c6090
Sherry <sherry@lulainno.com>
2021-07-25 02:33:59 +0800

    update letter

--- data.js
+++ data.js
@@ -1,3 +1,38 @@
-lastUpdate = "~ 2021/06/24 6AM";
+lastUpdate = "~ 2021/07/25 6AM";
 packages = `
+From:郵寄招領
+To:鄧翔文
+Room:508
+Color:白色單張
+
+From:郵寄招領
+To:黃玉靚
+Room:508
+Color:白色單張
+
+From:東海大學註冊組
+To:廖育賢
+Room:403
+Color:白色信封
+
+From:東海大學註冊組
+To:馬智宣
+Color:白色信封
+
+From:東海大學註冊組
+To:劉天仲
+Room:204
+Color:白色信封
+
+From:元大金控
+To:吳文軒
+Room:408
+Color:白色信封
 `;
+
+/*
+From:
+To:
+Room:
+Color:
+*/
