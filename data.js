73e2eb747a1761901b6a9c05d097e6d0f3ef2f23
SherryYueChiu <sherryyue.c@protonmail.com>
2021-07-29 18:56:02 +0800

    update etter

--- data.js
+++ data.js
@@ -1,21 +1,12 @@
-lastUpdate = "~ 2021/07/29 6AM";
+lastUpdate = "~ 2021/07/30 6AM";
 packages = `
 From:郵寄招領
 To:黃玉靚
 Color:白色單張
 
-From:東海大學註冊組
-To:廖育賢
-Room:403
-Color:白色信封
-
-From:東海大學註冊組
-To:馬智宣
-Color:白色信封
-
-From:元大金控
-To:吳文軒
-Room:408
+From:玉山銀行
+To:陳昱廷
+Room:505
 Color:白色信封
 `;
 
