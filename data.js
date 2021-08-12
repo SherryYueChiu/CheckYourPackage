faa2af0959f80b524108cc773442a85c92f618c0
SherryYueChiu <sherryyue.c@protonmail.com>
2021-08-13 02:50:15 +0800

    update letter

--- data.js
+++ data.js
@@ -1,24 +1,13 @@
-lastUpdate = "~ 2021/08/06 6AM";
+lastUpdate = "~ 2021/08/13 6AM";
 packages = `
-From:郵寄招領
-To:陳靜如
-Room:504
-Color:白色單張
+From:玉山銀行
+To:蘇斳
+Room:310
+Color:白色信封
 
 From:郵寄招領
 To:黃玉靚
 Color:白色單張
-
-From:玉山銀行
-To:陳昱廷
-Room:505
-Color:白色信封
-
-To:陳靜如
-Room:504
-Color:牛皮紙袋
-
-
 `;
 
 /*
